9574632c47acb3cb31602c398f2eb1cb7678160e
engcfraposo <engcfraposo@gmail.com>
2020-04-14 03:50:08 -0300

    update 14/04/2020

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,31 @@
+const parseStringAsArray = require('../utils/parseStringAsArray');
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async index(request, response) {
+
+        const {latitude, longitude, techs} = request.query
+
+        const techsArray = parseStringAsArray(techs);
+
+        const  devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [ longitude , latitude ],
+                    },
+                    $maxDistance: 1000000000,
+                },
+            },
+        });
+       
+
+        return response.json({devs})
+    }
+    
+
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,41 +1,15 @@
 const { Router } = require('express');
-const axios = require('axios');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
-const Dev = require('./models/Dev');
 
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    const {github_username, techs} = request.body
-    
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
-    const { login, avatar_url, html_url, bio, latitude, longitude } = apiResponse.data;
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    
-        
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-
-    };
-
-    console.log(location)
-
-    const dev = await Dev.create({
-        github_username,
-        name : login,
-        avatar_url,
-        html_url,
-        bio,
-        techs : techsArray,
-        location 
-    });
-
-    return response.json(dev)
-});
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
